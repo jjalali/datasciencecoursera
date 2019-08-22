d8afef3a597482030bda8cdd20bc6ed171e5ede5
Jalal Jalali <53275787+jjalali@users.noreply.github.com>
2019-08-22 08:52:21 -0400

    Add files via upload

---
+++ Course 3 - Getting and Cleaning Data/Week 4/CodeBook.md
@@ -0,0 +1,43 @@
+---
+title: "CodeBook"
+author: "Jalal Jalali"
+date: "August 14, 2019"
+output: html_document
+---
+
+## CodeBook
+
+**timebodyaccmean (x,y,z)**     time body accelerometer mean, in x, y, or z direction  
+**timebodyaccstd (x,y,z)**      time body accelerometer standard deviation, in x, y, or z direction  
+**timegravityaccmean (x,y,z)**  time gravity accelerometer mean, in x, y, or z direction  
+**timegravityaccstd (x,y,z)**   time gravity accelerometer standard deviation, in x, y, or z direction  
+**timebodyaccjerkmean (x,y,z)** time body accelerometer jerkmean, in x, y, or z direction  
+**timebodyaccjerkstd (x,y,z)**  time body accelerometer jerkstandard deviation, in x, y, or z direction  
+**timebodygyromean (x,y,z)**        time body gyroscope mean, in x, y, or z direction  
+**timebodygyrostd (x,y,z)**         time body gyroscope standard deviation, in x, y, or z direction  
+**timebodygyrojerkmean (x,y,z)**	time body gyroscope jerk mean, in x, y, or z direction  
+**timebodygyrojerkstd (x,y,z)**	time body gyroscope jerk standard deviation, in x, y, or z direction  
+**timebodyaccmagmean**	        time body accelerometer magnitude mean  
+**timebodyaccmagstd**	        time body accelerometer magnitude standard deviation  
+**timegravityaccmagmean**	        time gravity accelerometer magnitudemean  
+**timegravityaccmagstd**    	time gravity accelerometer magnitudestandard deviation  
+**timebodyaccjerkmagmean**  	time body accelerometer jerk magnitude mean  
+**timebodyaccjerkmagstd**	        time body accelerometer jerk magnitude standard deviation  
+**timebodygyromagmean**	        time body gyroscope magnitude mean  
+**timebodygyromagstd**      	time body gyroscope magnitude standard deviation  
+**timebodygyrojerkmagmean**	        time body gyroscope jerk magnitude mean  
+**timebodygyrojerkmagstd**	        time body gyroscope jerk magnitude standard deviation  
+**freqbodyaccmean (x,y,z)**	        frequencybody accelerometer mean, in x, y, or z direction  
+**freqbodyaccstd (x,y,z)**	        frequencybody accelerometer standard deviation, in x, y, or z direction  
+**freqbodyaccjerkmean (x,y,z)**	frequencybody accelerometer jerk mean, in x, y, or z direction  
+**freqbodyaccjerkstd (x,y,z)**	frequencybody accelerometer jerk standard deviation, in x, y, or z direction  
+**freqbodygyromean (x,y,z)**	frequencybody gyroscope mean, in x, y, or z direction  
+**freqbodygyrostd (x,y,z)**	        frequencybody gyroscope standard deviation, in x, y, or z direction  
+**freqbodyaccmagmean**	        frequencybody accelerometer magnitude mean  
+**freqbodyaccmagstd**	        frequencybody accelerometer magnitude standard deviation  
+**freqbodybodyaccjerkmagmean**	frequencybody accelerometer jerk magnitude mean  
+**freqbodybodyaccjerkmagstd**	frequencybody accelerometer jerk magnitude standard deviation  
+**freqbodybodygyromagmean**	        frequencybody gyroscope magnitude mean  
+**freqbodybodygyromagstd**	        frequencybody gyroscope magnitude standard deviation  
+**freqbodybodygyrojerkmagmean**	frequencybody gyroscope jerk magnitude mean  
+**freqbodybodygyrojerkmagstd**	frequencybody gyroscope jerk magnitude standard deviation  
--- Course 3 - Getting and Cleaning Data/Week 4/run_analysis.R
+++ Course 3 - Getting and Cleaning Data/Week 4/run_analysis.R
@@ -10,145 +10,116 @@ rm(list = ls())
 
 library(tidyverse)
 
-# Read train data into train_df
-train_df <- read.table("X_train.txt", header = FALSE, sep = "")
-# train_df <- tbl_df(train)
-# rm(train)
 
-# read test data file into test_df
-test_df <- read.table("X_test.txt", header = FALSE, sep = "")
-# test_df <- tbl_df(test)
-# rm(test)
+# download the zip file
+zip_file_path <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+temp <- tempfile()
+download.file(zip_file_path,temp)
+unzip(temp)
 
-# merge the two data sets into merged_df
-# merged_df <- rbind(train, test)
-merged_df <- rbind(train_df, test_df)
-# merged_df <- tbl_df(rbind(train_df, test_df))
+# Read train data 
+X_train <- read.table("./UCI HAR Dataset/train/X_train.txt", header = FALSE, sep = "")
 
-# import the feature names
-features_orig <- read_tsv("features.txt", col_names = FALSE)
+y_train <- read.table("./UCI HAR Dataset/train/y_train.txt", header = FALSE, sep = "")
 
-# extract the feature name and discard the row number
-features_orig <- separate(features_orig, X1, c(NA, "feature"), " ")
+subject_train <- read.table("./UCI HAR Dataset/train/subject_train.txt", header = FALSE, sep = "")
 
-# remove special characters from the feature names
-features <- features_orig  %>%
-        # update the movements from numbers to more meaningful names - X direction
-        sapply(str_replace_all, "\\)1", "\\)_walking") %>%
-        sapply(str_replace_all, "\\)2", "\\)_walking_up") %>%
-        sapply(str_replace_all, "\\)3", "\\)_walking_dn") %>%
-        sapply(str_replace_all, "\\)4", "\\)_sitting") %>%
-        
+
+# read test data
+X_test <- read.table("./UCI HAR Dataset/test/X_test.txt", header = FALSE, sep = "")
+
+y_test <- read.table("./UCI HAR Dataset/test/y_test.txt", header = FALSE, sep = "")
+
+subject_test <- read.table("./UCI HAR Dataset/test/subject_test.txt", header = FALSE, sep = "")
+
+
+# read activity labels and features
+activity_labels <- read.table("./UCI HAR Dataset/activity_labels.txt", header = FALSE, sep = "")
+
+features <- read.table("./UCI HAR Dataset/features.txt", header = FALSE, sep = "")
+
+
+# add column names
+y_train <- rename(y_train, activity = V1)
+y_test <- rename(y_test, activity = V1)
+
+subject_train <- rename(subject_train, subject_id = V1)
+subject_test <- rename(subject_test, subject_id = V1)
+
+subject_data <- rbind(subject_train, subject_test)
+
+# merge training and testing datasets
+X_data <- rbind(X_train, X_test)
+
+# merge training and test labels
+y_data <- rbind(y_train, y_test)
+
+# get column numbers that contain mean() and std() from the features table
+mean_std_cols <- features %>% 
+        filter(str_detect(V2, "[Mm]ean\\(\\)|[Ss]td\\(\\)"))
+
+# extract column numbers that have mean() and std()
+mean_std_cols <- as.numeric(unlist(mean_std_cols[1]))
+
+# create a subset of the data that are only mean() and std() of measurements
+subset_mean_std <- X_data[mean_std_cols]
+# write.table(subset_mean_std, "mean_std.txt", row.names = FALSE)
+
+
+# use descriptive activity names for activites
+
+# help from http://stat545.com/bit008_lookup.html
+act <- tbl_df(activity_labels)
+indices <- match(x = y_data$activity, table = act$V1)
+
+# add activity labels to Data
+subset_mean_std <- subset_mean_std %>% 
+        mutate(activity = act$V2[indices] )
+
+# cleaning the variable names, removing special characters and making 
+# everything lowercase
+features[,2] <- features[,2] %>%
         # remove parentheses, comma, and dash
         sapply(str_replace_all, "\\)", "") %>%
         sapply(str_replace_all, "\\(", "") %>%
         sapply(str_replace_all, "\\,", "") %>%
         sapply(str_replace_all, "-", "") %>%
-        
-        sapply(str_replace_all, "X1", "x_walking") %>%
-        sapply(str_replace_all, "X2", "x_walking_up") %>%
-        sapply(str_replace_all, "X3", "x_walking_dn") %>%
-        sapply(str_replace_all, "X4", "x_sitting") %>%
-        
-        # update the movements from numbers to more meaningful names - Y direction
-        sapply(str_replace_all, "Y1", "y_walking") %>%
-        sapply(str_replace_all, "Y2", "y_walking_up") %>%
-        sapply(str_replace_all, "Y3", "y_walking_dn") %>%
-        sapply(str_replace_all, "Y4", "y_sitting") %>%
-
-        # update the movements from numbers to more meaningful names - Z direction
-        sapply(str_replace_all, "Z1", "z_walking") %>%
-        sapply(str_replace_all, "Z2", "z_walking_up") %>%
-        sapply(str_replace_all, "Z3", "z_walking_dn") %>%
-        sapply(str_replace_all, "Z4", "z_sitting") %>%
+        sapply(str_replace_all, "-", "") %>%
+        # repalce t with time and f with freq, suggested by
+        # https://drive.google.com/file/d/0B1r70tGT37UxYzhNQWdXS19CN1U/view
+        sapply(str_replace, "^t", "time") %>%
+        sapply(str_replace, "^f", "freq") %>%
         
         tolower()
 
-features_orig <- tbl_df(features_orig)
-features <- tbl_df(features)
+# get the column names from the dataset
+new_labels <- names(subset_mean_std[1:(ncol(subset_mean_std)-1)])
 
-# direction is missing from some parameters, add them
-for (i in 303:316) {
-        features[i,1] <- paste0(features[i,1], "_x")
-}
+# remove the "V" from the names. the remaining will be the row number 
+# in the features table that contains the corrent feature name
+new_labels <- sapply(new_labels, substr, 2,4)
+new_labels <- as.vector(new_labels)
 
-for (i in 317:330) {
-        features[i,1] <- paste0(features[i,1], "_y")
-}
-
-for (i in 331:344) {
-        features[i,1] <- paste0(features[i,1], "_z")
-}
-
-for (i in 382:395) {
-        features[i,1] <- paste0(features[i,1], "_x")
-}
-
-for (i in 396:409) {
-        features[i,1] <- paste0(features[i,1], "_y")
-}
-
-for (i in 410:423) {
-        features[i,1] <- paste0(features[i,1], "_z")
-}
-
-for (i in 461:474) {
-        features[i,1] <- paste0(features[i,1], "_x")
-}
-
-for (i in 475:488) {
-        features[i,1] <- paste0(features[i,1], "_y")
-}
-
-for (i in 489:502) {
-        features[i,1] <- paste0(features[i,1], "_z")
+# update the subset_mean_std column names with the names from features
+for (i in 1:(ncol(subset_mean_std)-1)) {
+        # m <- as.numeric(new_labels[i])
+        colnames(subset_mean_std)[i] <- features[as.numeric(new_labels[i]),2]
 }
 
 
-# convert features to a vector
-features <- as.list(features) %>%
-        transpose %>%
-        unlist
+# add the subject id to the data
+subset_mean_std <- cbind(subset_mean_std, subject_data)
 
-        # unlist(transpose(as.list(features)))
-
-# convert data to a data frame
-merged_df <- as.data.frame(merged_df)
-
-# update column names of the data frame
-names(merged_df) <- features
-
-# make a tibble
-merged_df <- tbl_df(merged_df)
-
-
-# extract only mean and standard deviation and save in data_stats
-data_stats <- merged_df %>%
-        select(matches('mean|std'))
-
-
-# import subject IDs for both training and testing sets
-subject_train <- read.table("subject_train.txt", header = FALSE, sep = "")
-
-subject_test <- read.table("subject_test.txt", header = FALSE, sep = "")
-
-# merge the two tables
-subject_merged <- rbind(subject_train, subject_test)
-
-# create another table with data and subject IDs
-id_data <- cbind(merged_df, subject_merged)
-
-# rename the column to subject_id
-id_data <- rename(id_data, subject_id = V1)
-
-id_data <- select(id_data, matches('walking|walking_up|walking_dn|sitting|subject_id'))
-
-subject_avg <- id_data %>%
-        group_by(subject_id) %>%
+# summarize based on subject id then activity
+tidy_data <- subset_mean_std %>%
+        group_by(subject_id, activity) %>%
         summarise_all(mean)
 
-write.table(subject_avg, "tidy_data.txt", row.names = FALSE)
+# write the tidy data to a txt file
+write.table(tidy_data, "tidy_data.txt", row.names = FALSE)
 
-rm(i, train_df, test_df, subject_train, subject_test)
 
+# remove files that are no longer needed
+rm(y_train, y_test, X_train, X_test, subject_train, subject_test, act, 
+   activity_labels, X_data, y_data, i, indices, new_labels,subject_data)
 
